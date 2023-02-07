f65b99e118b50325dda36d3d50d25ec3903289a8
M.Mostova <mmainfo15@gmail.com>
2023-02-07 19:30:58 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,20 +5,18 @@ const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-pageReload()
+pageReload();
 
 const onPlay = function ({ seconds }) {
   localStorage.setItem(LOCALSTORAGE_KEY, seconds);
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
-//player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
-
 
 function pageReload(event) {
   const storageValue = localStorage.getItem(LOCALSTORAGE_KEY);
-  if (storageValue) {
-    player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (!storageValue) {
+    return;
   }
-  return;
+  player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
 }
