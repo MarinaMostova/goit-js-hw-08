cb271753eba96aa12bfb9032a5271654e37f1d50
M.Mostova <mmainfo15@gmail.com>
2023-02-07 11:38:00 +0200

    Домашнє завдання 8

---
+++ src/js/01-gallery.js
@@ -0,0 +1,34 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+const markupEl = createImageGalleryMarkup(galleryItems);
+galleryList.innerHTML = markupEl;
+
+function createImageGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}" 
+  alt="${description}" 
+  />
+</a>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
